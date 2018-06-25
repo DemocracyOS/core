a3eb4da4084eade4e168f770de0746e955fc1e2a
Guillermo Croppi <guillermocroppi@gmail.com>
2018-06-25 17:17:37 -0300

    Added db-api tests for Community. Updates #16

--- test/api/community.js
+++ test/api/community.js
@@ -15,49 +15,50 @@ chai.use(chaiHttp)
 let agent = null // user agent, simulates a browser.
 let bearerToken = null // bearer token provided by Keycloak
 
-describe('Community API (/api/v1/community)', () => {
-  before(async () => {
-    await require('../../server')
-    // Before starting the tests, make sure to prepare the enviroment
-    // That is, clean the database, or initialize anything you need before executing the suite of tests
-    await Community.remove({})
-  })
+// describe('Community API (/api/v1/community)', () => {
+//   before(async () => {
+//     await require('../../server')
+//     // Before starting the tests, make sure to prepare the enviroment
+//     // That is, clean the database, or initialize anything you need before executing the suite of tests
+//     await Community.remove({})
+//   })
 
-  describe('Log in as an authorized user', () => {
-    before(async () => {
-      // Log In as aser
-      agent = await chai.request.agent('http://localhost:3000')
-    })
-    it('should get bearer token and save it', async () => {
-      // await agent.get('/auth/csrf')
-      //   .then((res) => {
-      //     // TODO
-      //   })
-      //   .catch((err) => {
-      //     throw err
-      //   })
-    })
-    it('GET / | any user should be able to get the data of a community', async () => {
-      await agent.get('/api/v1.0/settings')
-        .then((res) => {
-          // expect(res).to.have.status(OK)
-          // TODO
-        })
-        .catch((err) => {
-          throw err
-        })
-    })
-    it('PUT / should be able to modify at least the name of a setting', async () => {
-      await agent.post('/api/v1.0/settings')
-        // .set('X-CSRF-TOKEN', csrfToken)
-        // .set('X-Requested-With', 'XMLHttpRequest')
-        // .set('Content-Type', 'application/x-www-form-urlencoded')
-        // .send(samplePostSetting)
-        .then((res) => {
-          /* eslint-disable no-unused-expressions */
-        })
-        .catch((err) => {
-          throw err
-        })
-  })
-})
+//   describe('Log in as an authorized user', () => {
+//     before(async () => {
+//       // Log In as aser
+//       agent = await chai.request.agent('http://localhost:3000')
+//     })
+//     it('should get bearer token and save it', async () => {
+//       // await agent.get('/auth/csrf')
+//       //   .then((res) => {
+//       //     // TODO
+//       //   })
+//       //   .catch((err) => {
+//       //     throw err
+//       //   })
+//     })
+//     it('GET / any user should be able to get the data of a community', async () => {
+//       await agent.get('/api/v1.0/settings')
+//         .then((res) => {
+//           // expect(res).to.have.status(OK)
+//           // TODO
+//         })
+//         .catch((err) => {
+//           throw err
+//         })
+//     })
+//     it('PUT / should be able to modify at least the name of a setting', async () => {
+//       await agent.post('/api/v1.0/settings')
+//         // .set('X-CSRF-TOKEN', csrfToken)
+//         // .set('X-Requested-With', 'XMLHttpRequest')
+//         // .set('Content-Type', 'application/x-www-form-urlencoded')
+//         // .send(samplePostSetting)
+//         .then((res) => {
+//           /* eslint-disable no-unused-expressions */
+//         })
+//         .catch((err) => {
+//           throw err
+//         })
+//     })
+//   })
+// })
--- test/db-api/community.js
+++ test/db-api/community.js
@@ -8,13 +8,80 @@ const Community = require('../../models/community')
 const community = require('../../db-api/community')
 
 // Make a sample
-// const communitySample = { }
+const communitySample = {
+  name: 'A testing community',
+  mainColor: '#425cf4',
+  logo: null,
+  user: null,
+  initialized: true
+}
 
 describe('Community DB-APIs', () => {
-  it('Community.get() should get the only one community', () => {
-    
-  })
-  it('Community.update() should update the only one community', () => {
+  // ===================================================
+  it('Community.create() should create a community', () => {
+    // require module with rewire to override its internal Settings reference
+    const community = rewire('../../db-api/community')
+    // replace Setting constructor for a spy
+    const CommunityMock = sinon.spy()
 
+    // add a findOne method that only returns null
+    CommunityMock.findOne = () => { return Promise.resolve(null) }
+
+    // add a save method that only returns the data
+    CommunityMock.prototype.save = () => { return Promise.resolve(communitySample) }
+
+    // create a spy for the findOne method
+    sinon.spy(CommunityMock.findOne)
+    // create a spy for the save method
+    const save = sinon.spy(CommunityMock.prototype, 'save')
+
+    // override Setting inside `db-api/community`
+    community.__set__('Community', CommunityMock)
+    // call create method
+    return community.create(communitySample)
+      .then((result) => {
+        sinon.assert.calledWithNew(CommunityMock)
+        sinon.assert.calledWith(CommunityMock, communitySample)
+        sinon.assert.calledOnce(save)
+        expect(result).to.equal(communitySample)
+      })
+  })
+  // ===================================================
+  it('Community.get() should get the only community created in the database', () => {
+    const CommunityMock = sinon.mock(Community)
+
+    CommunityMock
+      .expects('findOne').withArgs({})
+      .chain('exec')
+      .resolves(communitySample)
+
+    return community.get()
+      .then((result) => {
+        CommunityMock.verify()
+        CommunityMock.restore()
+        expect(result).to.equal(communitySample)
+      })
+  })
+  // ===================================================
+  it('Community.update() should update the only community created in the database', () => {
+    const CommunityMock = sinon.mock(Community)
+    const save = sinon.spy(() => communitySample)
+    const changedSample = communitySample
+    const newName = 'Another Name'
+    // Change the community name
+    changedSample.name = newName
+    
+    CommunityMock
+      .expects('findOne').withArgs({})
+      .chain('exec')
+      .resolves({ save })
+
+    return community.update(changedSample)
+      .then((result) => {
+        CommunityMock.verify()
+        CommunityMock.restore()
+        sinon.assert.calledOnce(save)
+        expect(result.name).to.be.equal(newName)
+      })
   })
 })
