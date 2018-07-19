0634ce44ded332cfbf730a6eeec1418aee218409
Guillermo Croppi <guillermocroppi@gmail.com>
2018-07-19 13:32:18 -0300

    Added some faker methods for document types (for test purposes)

--- test/fake.js
+++ test/fake.js
@@ -11,6 +11,83 @@ const community = () => {
   }
 }
 
+const jsonSchemaProperties = (valid) => {
+  let fields = {
+    properties: {
+      'checked': {
+        'type': 'boolean',
+        'title': faker.lorem.words(5),
+        'description': faker.lorem.sentence(10),
+        'default': false
+      },
+      'dimensions': {
+        'type': 'object',
+        'properties': {
+          'width': {
+            'type': 'integer',
+            'title': faker.lorem.words(5),
+            'description': faker.lorem.sentence(10),
+            'default': 0
+          },
+          'height': {
+            'type': 'integer',
+            'title': faker.lorem.words(5),
+            'description': faker.lorem.sentence(10),
+            'default': 0
+          }
+        }
+      },
+      'id': {
+        'type': 'integer',
+        'title': faker.lorem.words(5),
+        'description': faker.lorem.sentence(10),
+        'default': 0
+      },
+      'name': {
+        'type': 'string',
+        'title': faker.lorem.words(5),
+        'description': faker.lorem.sentence(10),
+        'default': ''
+      },
+      'price': {
+        'type': 'number',
+        'title': faker.lorem.words(5),
+        'description': faker.lorem.sentence(10),
+        'default': 0
+      },
+      'tags': {
+        'type': 'array',
+        'items': {
+          'type': 'string',
+          'title': faker.lorem.words(5),
+          'description': faker.lorem.sentence(10),
+          'default': ''
+        }
+      }
+    }
+  }
+  
+  if(valid) {
+    return fields
+  }
+  
+  fields.properties.checked.default = 0;
+  fields.properties.dimensions.height.default = 'fake';
+
+  return fields
+}
+
+const documentType = () => {
+  return {
+    name: faker.lorem.words(4),
+    icon: faker.lorem.word(),
+    description: faker.lorem.sentence(10),
+    fields: jsonSchemaProperties()
+  }
+}2
+
 module.exports = {
-  community
+  community,
+  documentType,
+  jsonSchemaProperties
 }
