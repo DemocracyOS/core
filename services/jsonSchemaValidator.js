6fc898257c18afd681d4ef65743a8bf73de8e49c
Guillermo Croppi <guillermocroppi@gmail.com>
2018-07-23 12:18:49 -0300

    Added error display for Invalid JSON Schema error

--- services/jsonSchemaValidator.js
+++ services/jsonSchemaValidator.js
@@ -5,8 +5,9 @@ const Error = require('./errors')
 
 const isSchemaValid = (schema) => {
   let validSchema = ajv.validateSchema(schema)
-  console.log('Is this valid? ' + validSchema)
-  if (!validSchema) throw Error.ErrInvalidJSONSchema
+  // console.log('Is this valid? ' + validSchema)
+  // console.log(ajv.errorsText(validSchema.errors, { separator: '\n' }))
+  if (!validSchema) throw Error.ErrInvalidJSONSchema(ajv.errors)
 }
 
 const isDataValid = (schema, data) => {
