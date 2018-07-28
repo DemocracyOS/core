3eab1832497148f03dcf0a3b497cb4a571023f25
Guillermo Croppi <guillermocroppi@gmail.com>
2018-07-28 00:28:24 -0300

    Fixed documentTypeVersion dbApi

--- db-api/documentTypeVersion.js
+++ db-api/documentTypeVersion.js
@@ -1,5 +1,5 @@
 const { Types: { ObjectId } } = require('mongoose')
-const { ErrNotFound } = require('../services/errors')
+const { APIError, ErrNotFound } = require('../services/errors')
 const DocumentTypeVersion = require('../models/documentTypeVersion')
 // const log = require('../services/logger')
 const validator = require('../services/jsonSchemaValidator')
@@ -9,24 +9,12 @@ exports.get = function get (id) {
   return DocumentTypeVersion.findOne({ _id: ObjectId(id) })
 }
 
-exports.getVersion = function getByVersion (refId, version) {
-  // if version is 0, version-2 = -2, and it refers to the refVersion = null (first version)
-  // if version is 1, version-2 = -1, and it refers to the refVersion = null (first version)
-  // if version is 2, then version-2 = 0 and it refers to the refVersion != null and refVersion = 0 (second version)
-  let query = {
-    refId: ObjectId(refId)
+exports.getVersion = function getVersion (version) {
+  if (version < 0) {
+    throw APIError('Invalid version number')
   }
-  switch (version - 2) {
-    case -2:
-    case -1:
-      query.refVersion = null
-      break
-    default:
-      query.refVersion = version - 2
-      break
-  }
-  return DocumentTypeVersion.findOne(query).then((documentType) => {
+  return DocumentTypeVersion.findOne({}).then((documentType) => {
     if (!documentType) throw ErrNotFound(`DocumentType version not found`)
-    return documentType
+    return documentType.versions[version]
   })
 }
