103daba76e28eecf523915c50f91f63ca584b2b1
Guillermo Croppi <guillermocroppi@gmail.com>
2018-07-28 00:29:40 -0300

    Changed collection name

--- models/documentTypeVersion.js
+++ models/documentTypeVersion.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // Define `Community` Schema
-const DocumentTypeVersion = new mongoose.Schema({}, { collection: 'documenttypes_v', strict: false })
+const DocumentTypeVersion = new mongoose.Schema({}, { collection: 'documenttypes_versions', strict: false })
 
 // Expose `Community` Model
 module.exports = mongoose.model('DocumentTypeVersion', DocumentTypeVersion)
