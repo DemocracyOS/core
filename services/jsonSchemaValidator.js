32985ebf5a3daf646d9ac3259b1764c17879807f
Guillermo Croppi <guillermocroppi@gmail.com>
2018-08-03 12:23:06 -0300

    log.debug for jsonSchemaValidator

--- services/jsonSchemaValidator.js
+++ services/jsonSchemaValidator.js
@@ -2,26 +2,26 @@ const Ajv = require('ajv')
 // const localize = require('ajv-i18n')
 const ajv = Ajv({ allErrors: true })
 const Error = require('./errors')
+const log = require('./logger')
 
 const isSchemaValid = (schema) => {
   let validSchema = ajv.validateSchema(schema)
   // console.log('Is this valid? ' + validSchema)
   // console.log(ajv.errorsText(validSchema.errors, { separator: '\n' }))
+  log.debug({ validSchema: validSchema })
   if (!validSchema) throw Error.ErrInvalidJSONSchema(ajv.errors)
 }
 
 const isDataValid = (docTypeFields, data) => {
-  console.log(docTypeFields)
-  console.log(data)
   let validate = ajv.compile({
     properties: docTypeFields.properties,
     additionalProperties: false,
     required: docTypeFields.required
   })
   let valid = validate(data)
-  console.log('Is this valid data? ' + valid)
+  log.debug({ validData: valid })
   if (!valid) throw Error.ErrInvalidData(validate.errors)
-  return true;
+  return true
 }
 
 module.exports = {
