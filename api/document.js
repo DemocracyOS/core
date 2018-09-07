65e9be8a023f147ee68335446fa73b15f9bd8450
Guillermo Croppi <guillermocroppi@gmail.com>
2018-09-07 17:26:56 -0300

    Added api doc for the general comments and fixed the get api for documents

--- api/document.js
+++ api/document.js
@@ -3,17 +3,26 @@ const express = require('express')
 const Document = require('../db-api/document')
 const GeneralComment = require('../db-api/generalComment')
 const CustomForm = require('../db-api/customForm')
-// const CustomFormVersion = require('../db-api/customFormVersion')
 const router = express.Router()
 const auth = require('../services/auth')
 const errors = require('../services/errors')
-const log = require('../services/logger')
 const middlewares = require('../services/middlewares')
-// const {
-//   ErrMissingParam,
-//   ErrNotFound,
-//   ErrParamTooLong
-// } = require('../services/errors')
+
+/**
+ * @apiDefine admin User access only
+ * User must be an admin (Keycloak)
+ */
+
+/**
+ * @apiDefine accountable Accountable members only
+ * User must be a member of the Accountable group (Keycloak)
+ */
+
+/**
+ * @apiDefine authenticated Must be authenticated
+ * User must be authenticated before accessing (Keycloak)
+ */
+
 router.route('/')
   /**
    * @api {get} /documents List
@@ -22,15 +31,21 @@ router.route('/')
    * @apiGroup Document
    */
   .get(
-    // auth.keycloak.protect('realm:accountable'),
     async (req, res, next) => {
       try {
-        const permissions = auth.getPermissions(req)
-        console.log(permissions)
-        const results = await Document.list({}, {
-          limit: req.query.limit,
-          page: req.query.page
-        })
+        let results = null
+        // If it is null, just show the published documents
+        if (req.query.myDocs !== undefined && auth.hasRealmRole(req, 'accountable')) {
+          results = await Document.list({ author: req.session.user._id }, {
+            limit: req.query.limit,
+            page: req.query.page
+          }) 
+        } else {
+          results = await Document.list({ published: true }, {
+            limit: req.query.limit,
+            page: req.query.page
+          })
+        }
         res.status(status.OK).json({
           results: results.docs,
           pagination: {
@@ -48,6 +63,7 @@ router.route('/')
    * @apiName postDocument
    * @apiDescription Creates a document and returns the created document. The author is not required to be sent on the body. API sets the author by itself.
    * @apiGroup Document
+   * @apiPermission accountable
    */
   .post(
     auth.keycloak.protect('realm:accountable'),
@@ -78,7 +94,6 @@ router.route('/:id')
    * @apiSuccess {String}  author  The user id of the author.
    * @apiSuccess {String}  published State of the document. If `false` is a draft and should not be public.
    * @apiSuccess {String}  customForm Id of the custom form
-   * @apiSuccess {Integer}  customFormVersion The current version of the custom form. Starts with 0. If it is <code>0</code> then its the first version of the custom form.
    * @apiSuccess {Date}  createdAt Date of creation
    * @apiSuccess {Date}  updatedAt Date of update
    * @apiSuccess {Object}  content Content of the document
@@ -114,7 +129,7 @@ router.route('/:id')
    * @apiName putDocument
    * @apiDescription Modifies a document. You just need to send the changed fields. No need to send all the document.
    * @apiGroup Document
-   *
+   * @apiPermission accountable
    * @apiParam {Number} id Documents ID.
    */
   .put(
@@ -141,8 +156,8 @@ router.route('/:id')
    * @api {delete} /documents/:id Delete
    * @apiName deleteDocument
    * @apiGroup Document
+   * @apiPermission accountable
    * @apiDescription Deletes a document and returns the id of the removed document.
-   * @apiParam {Number} id Documents ID.
    */
   .delete(
     middlewares.checkId,
@@ -162,11 +177,21 @@ router.route('/:id')
     })
 
 router.route('/:id/comments')
+  /**
+   * @api {get} /documents/:id/comments Get
+   * @apiName getGeneralComments
+   * @apiGroup GeneralComments
+   * @apiDescription Retrieves a paginated list of comments of a document
+   */
   .get(
     middlewares.checkId,
     async (req, res, next) => {
       try {
-        const results = await Document.listGeneralComments(req.params.id, {
+        let query = {
+          document: req.params.id
+        }
+        if (req.body.field) query.field = req.body.field
+        const results = await Document.listGeneralComments(query, {
           limit: req.query.limit,
           page: req.query.page
         })
@@ -183,6 +208,20 @@ router.route('/:id/comments')
       }
     }
   )
+  /**
+   * @api {post} /documents/:id/comments Create
+   * @apiName createComment
+   * @apiGroup GeneralComments
+   * @apiDescription Creates a comment on a specific field of a document.
+   * @apiPermission authenticated
+   * @apiParam {string} field (Body) The field of the document where the comment is being made
+   * @apiParam {Number} comment (Body) The field of the document where the comment is being made
+   * @apiExample {json} POST body
+   * {
+   *  "field": "authorName",
+   *  "comment": "Nullam sit amet ipsum id metus porta rutrum in vel nibh. Sed efficitur quam urna, eget imperdiet libero ornare."
+   * }
+   */
   .post(
     middlewares.checkId,
     auth.keycloak.protect(),
