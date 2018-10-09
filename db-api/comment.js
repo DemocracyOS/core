303854f900e25d33d87aa75daa4b2de0597b5cbc
Guillermo Croppi <guillermocroppi@gmail.com>
2018-10-09 15:17:50 -0300

    Added populate with user for comments

--- db-api/comment.js
+++ db-api/comment.js
@@ -11,7 +11,7 @@ exports.create = async function create (comment) {
 
 exports.getAll = function getAll (query) {
   return Comment
-    .find(query)
+    .find(query).populate('user')
 }
 
 exports.resolve = function resolve (query) {
