2ca859cd80f02f7d722e503a11163b8bd72cc3cf
Guillermo Croppi <guillermocroppi@gmail.com>
2018-10-12 15:47:33 -0300

    EsLint fix

--- db-api/like.js
+++ db-api/like.js
@@ -1,15 +1,15 @@
 const Like = require('../models/like')
-const { ErrNotFound } = require('../services/errors');
+const { ErrNotFound } = require('../services/errors')
 
-exports.get = function get(query) {
+exports.get = function get (query) {
   return Like.findOne(query)
 }
 
-exports.create = function create(likeData) {
+exports.create = function create (likeData) {
   return (new Like(likeData)).save()
 }
 
-exports.remove = function remove(id) {
+exports.remove = function remove (id) {
   return Like.findById(id)
     .then((like) => {
       if (!like) throw ErrNotFound('Like to remove not found')
