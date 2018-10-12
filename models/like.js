8b959cb6a2182532a059a8d728d647f5b80c0323
Pablo Mendez <mendez.developer@gmail.com>
2018-10-12 12:47:20 -0300

    added timestamps to Like model
    issue #47

--- models/like.js
+++ models/like.js
@@ -11,7 +11,7 @@ const Like = new mongoose.Schema({
     ref: 'Comment',
     required: true
   }
-})
+}, { timestamps: true })
 
 // Expose 'Like' model
 module.exports = mongoose.model('Like', Like)
