43ce96cc1710ba1a1d9633caa50e1a394e045e4a
Pablo Mendez <mendez.developer@gmail.com>
2018-10-14 11:55:37 -0300

    added when prop and logic for timeago
    issue #48

--- models/comment.js
+++ models/comment.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose')
 const mongoosePaginate = require('mongoose-paginate')
+const timeago = require('../services/timeago')
 
 const Comment = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   document: { type: mongoose.Schema.Types.ObjectId, ref: 'Document' },
   field: { type: String },
   content: { type: String },
-  resolved: { type: Boolean, default: false }
+  resolved: { type: Boolean, default: false },
+  when: { type: String }
 }, { timestamps: true })
+
+Comment.post('init', function(doc){
+  doc.when = timeago().format(doc.createdAt, 'es_AR')
+  return doc;
+})
 
 // Model's Plugin Extensions
 Comment.plugin(mongoosePaginate)
