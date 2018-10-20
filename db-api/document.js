0ee8afab9a2a561be8d287d8dc8f27c9fd33397d
Guillermo Croppi <guillermocroppi@gmail.com>
2018-10-20 15:32:34 -0300

    Now get, create, list document(s) works appending the content of its current version. Updates #58

--- db-api/document.js
+++ db-api/document.js
@@ -1,6 +1,7 @@
 const { Types: { ObjectId } } = require('mongoose')
 const { merge } = require('lodash/object')
 const Document = require('../models/document')
+const DocumentVersion = require('../models/documentVersion')
 const Comment = require('../models/comment')
 const validator = require('../services/jsonSchemaValidator')
 const errors = require('../services/errors')
@@ -10,28 +11,67 @@ exports.countAuthorDocuments = async function countAuthorDocuments (author) {
 }
 
 exports.isAuthor = async function isAuthor (id, author) {
-  let count = await Document.count({ _id: id, author: author })
+  let count = await Document.countDocuments({ _id: id, author: author })
   return count
 }
 
 // Create document
-exports.create = async function create (document, customForm) {
+exports.create = async function create (documentData, customForm) {
+  // Check if the data is valid
   validator.isDataValid(
     customForm.fields,
-    document.content.fields
+    documentData.content
   )
-  return (new Document(document)).save()
+  // Create a new document
+  let documentToSave = {
+    author: documentData.author,
+    customForm: customForm._id,
+    published: documentData.published,
+    lastVersion: 1
+  }
+  // Save the document, to get the id
+  let theDocument = await (new Document(documentToSave)).save()
+  // Create a new version
+  let versionToSave = {
+    document: theDocument._id,
+    version: 1,
+    content: documentData.content,
+    contributions: []
+  }
+
+  let theVersion = await (new DocumentVersion(versionToSave)).save()
+  theDocument.content = theVersion.content
+  return theDocument
 }
 
-// Get document
-exports.get = function get (query) {
-  return Document.findOne(query).populate('author')
+// Get document (with its last version)
+exports.get = async function get (query) {
+  let document = await Document.findOne(query).populate('author').lean()
+  let queryVersion = {
+    document: document._id,
+    version: document.lastVersion
+  }
+  let lastVersion = await DocumentVersion.findOne(queryVersion)
+  document.content = lastVersion.content
+  console.log(lastVersion)
+  return document
 }
 
 // List documents
-exports.list = function list (query, { limit, page }) {
-  return Document
-    .paginate(query, { page, limit })
+exports.list = async function list (query, { limit, page }) {
+  let documentList = await Document.paginate(query, { page, limit, lean: true })
+  let promisesPopulate = documentList.docs.map(async (doc) => {
+    let theVersion = await DocumentVersion.findOne({
+      document: doc._id,
+      version: doc.lastVersion
+    }).lean()
+    let aux = doc
+    aux.content = theVersion.content
+    return aux
+  })
+  let populatedDocs = await Promise.all(promisesPopulate)
+  documentList.docs = populatedDocs
+  return documentList
 }
 
 // Update document
@@ -46,7 +86,7 @@ exports.update = async function update (id, document, customForm) {
       // Validate the data
       validator.isDataValid(
         customForm.fields,
-        documentToSave.content.fields
+        documentToSave.content
       )
       // Save!
       return documentToSave.save()
@@ -61,7 +101,7 @@ exports.updateField = async function updateField (id, field, state, hash) {
       // Found?
       if (!_document) throw errors.ErrNotFound('Document to update not found')
       // Deep merge the change(s) with the document
-      _document.content.fields[field] = state
+      _document.content[field] = state
       _document.content.hashes[field] = hash
       // Validate the data
       // Save!
