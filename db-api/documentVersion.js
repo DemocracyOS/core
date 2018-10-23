531bc4dcb0fa0a67835efc1fe0d985e51bdc970a
Pablo Mendez <mendez.developer@gmail.com>
2018-10-23 09:09:51 -0300

    Created documentVersion update method
    issue #50

--- db-api/documentVersion.js
+++ db-api/documentVersion.js
@@ -7,3 +7,19 @@ const errors = require('../services/errors')
 exports.get = function get (query) {
   return DocumentVersion.findOne(query)
 }
+
+exports.update = async function update (id, content, customForm) {
+  return DocumentVersion.findOne({ _id: id })
+    .then(version => {
+      if (!version) throw errors.ErrNotFound('Version document to update not found')
+
+      let versionToSave = merge(version, content)
+
+      validator.isDataValid(
+        customForm.fields,
+        content
+      )
+
+      return versionToSave.save()
+    })
+}
