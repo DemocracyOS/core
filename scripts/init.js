eaf317daf5d626d5a5cf3b0a82ed113058181d53
Guillermo Croppi <guillermocroppi@gmail.com>
2018-10-25 12:48:17 -0300

    Updated init script

--- scripts/init.js
+++ scripts/init.js
@@ -5,10 +5,130 @@ const CustomForm = require('../models/customForm')
 const dbCustomForm = require('../db-api/customForm')
 const config = require('../config')
 const log = require('../services/logger')
-// const { NODE_ENV } = process.env
 
-// Error Definitions
-// class NoEnvDefined extends Error { }
+let communityData = {
+  name: config.SETUP.COMMUNITY_NAME,
+  mainColor: config.SETUP.COMMUNITY_COLOR,
+  logo: null,
+  user: null,
+  userProfileSchema: null,
+  initialized: true,
+  permissions: {
+    user: {}, // Permissions for users
+    accountable: { // Permissions for accountables
+      documentCreationLimit: 1
+    },
+    admin: {} // Permissions for admin
+  }
+}
+
+let userProfileCustomForm = {
+  name: 'User Profile',
+  icon: 'fas fa-user',
+  description: 'Template for a user profile',
+  version: 0,
+  fields: {
+    'blocks': [
+      {
+        'fields': [
+          'occupation'
+        ],
+        'name': 'About the user'
+      }
+    ],
+    'properties': {
+      'occupation': {
+        'type': 'string',
+        'title': 'User occupation'
+      }
+    },
+    'required': [],
+    'richText': [],
+    'allowComments': []
+  }
+}
+
+let projectCustomForm = {
+  fields: {
+    'blocks': [
+      {
+        'fields': [
+          'title',
+          'imgCover',
+          'youtubeId',
+          'fundation'
+        ],
+        'name': 'Project\'s basic info'
+      },
+      {
+        'fields': [
+          'articles'
+        ],
+        'name': 'Articles of the project'
+      }
+    ],
+    'properties': {
+      'title': {
+        'type': 'string',
+        'title': 'Project\'s title'
+      },
+      'imageCover': {
+        'anyof': [
+          { 'type': 'null' },
+          { 'type': 'string' }
+        ],
+        'title': 'URL for the cover of the image'
+      },
+      'fundation': {
+        'type': 'object',
+        'title': 'Project\'s fundations'
+      },
+      'articles': {
+        'type': 'object',
+        'title': 'Articles'
+      },
+      'youtubeId': {
+        'anyof': [
+          { 'type': 'null' },
+          { 'type': 'String' }
+        ],
+        'title': 'Youtube Video ID'
+      },
+      'closingDate': {
+        'oneOf': [
+          { 'type': 'null' },
+          { 'type': 'string' }
+        ],
+        'title': 'Closing date (to participate)'
+      },
+      'closure': {
+        'anyof': [
+          { 'type': 'null' },
+          { 'type': 'object' }
+        ],
+        'title': 'Closure of the document'
+      }
+    },
+    'required': [
+      'title',
+      'fundation',
+      'articles'
+    ],
+    'richText': [
+      'fundation',
+      'articles'
+    ],
+    'allowComments': [
+      'fundation',
+      'articles'
+    ]
+  },
+  name: 'Project',
+  icon: 'far fa-files',
+  description: 'This is the template of fields for projects',
+  version: 0
+}
+
 class DatabaseNotEmpty extends Error { }
 
 async function checkDB () {
@@ -20,153 +140,17 @@ async function checkDB () {
   log.debug('--> OK')
 }
 
-// async function checkEnv () {
-//   log.debug(`* Checking if ENV is defined... [${NODE_ENV}] defined`)
-//   if (NODE_ENV === undefined) throw new NoEnvDefined('ERROR You need to run the script with NODE_ENV, like "dev" or "prod"')
-//   log.debug('--> OK')
-// }
-
 async function startSetup () {
   try {
     await checkDB()
+    log.debug('* Creating user profile custom form...')
+    let profileSchema = await dbCustomForm.create(userProfileCustomForm)
     log.debug('* Creating community...')
-    let profileSchema = await dbCustomForm.create({
-      name: 'User Profile',
-      icon: 'fas fa-user',
-      description: 'Template for a user profile',
-      version: 0,
-      fields: {
-        'blocks': [
-          {
-            'fields': [
-              'twitter',
-              'facebook'
-            ],
-            'name': 'Social Media'
-          },
-          {
-            'fields': [
-              'bio'
-            ],
-            'name': 'About the user'
-          }
-        ],
-        'properties': {
-          'bio': {
-            'type': 'string',
-            'title': 'User information'
-          },
-          'twitter': {
-            'type': 'string',
-            'title': "User's surname"
-          },
-          'facebook': {
-            'type': 'string',
-            'title': "User's facebook"
-          }
-        },
-        'required': [],
-        'richText': [],
-        'allowComments': []
-      }
-    })
-    await dbCommunity.create({
-      name: config.SETUP.COMMUNITY_NAME,
-      mainColor: config.SETUP.COMMUNITY_COLOR,
-      logo: null,
-      user: null,
-      userProfileSchema: profileSchema._id,
-      initialized: true,
-      permissions: {
-        user: {}, // Permissions for users
-        accountable: { // Permissions for accountables
-          documentCreationLimit: 1
-        },
-        admin: {} // Permissions for admin
-      }
-    })
+    communityData.userProfileSchema = profileSchema._id
+    await dbCommunity.create(communityData)
     log.debug('--> OK')
-    log.debug('* Creating document type...')
-    await dbCustomForm.create({
-      fields: {
-        'blocks': [
-          {
-            'fields': [
-              'title',
-              'imgCover',
-              'youtubeId',
-              'fundation'
-            ],
-            'name': 'Project\'s basic info'
-          },
-          {
-            'fields': [
-              'articles'
-            ],
-            'name': 'Articles of the project'
-          }
-        ],
-        'properties': {
-          'title': {
-            'type': 'string',
-            'title': 'Project\'s title'
-          },
-          'imageCover': {
-            'anyof': [
-              { 'type': 'null' },
-              { 'type': 'string' }
-            ],
-            'title': 'URL for the cover of the image'
-          },
-          'fundation': {
-            'type': 'object',
-            'title': 'Project\'s fundations'
-          },
-          'articles': {
-            'type': 'object',
-            'title': 'Articles'
-          },
-          'youtubeId': {
-            'anyof': [
-              { 'type': 'null' },
-              { 'type': 'String' }
-            ],
-            'title': 'Youtube Video ID'
-          },
-          'closingDate': {
-            'oneOf': [
-              { 'type': 'null' },
-              { 'type': 'string' }
-            ],
-            'title': 'Closing date (to participate)'
-          },
-          'closure': {
-            'anyof': [
-              { 'type': 'null' },
-              { 'type': 'object' }
-            ],
-            'title': 'Closure of the document'
-          }
-        },
-        'required': [
-          'title',
-          'fundation',
-          'articles'
-        ],
-        'richText': [
-          'fundation',
-          'articles'
-        ],
-        'allowComments': [
-          'fundation',
-          'articles'
-        ]
-      },
-      name: 'Project',
-      icon: 'far fa-files',
-      description: 'This is the template of fields for projects',
-      version: 0
-    })
+    log.debug('* Creating document type custom form...')
+    await dbCustomForm.create(projectCustomForm)
     log.debug('--> OK')
     log.debug('--> Setup finished!')
     process.exit(0) // Success
